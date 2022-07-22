728718363cbd6adafe450e441e53213cf22321f2
ig-rudenko <ig.rudenko1@yandex.ru>
2022-07-22 15:37:44 +0300

    v 0.4.1 Модификация создания staticfiles

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 python ./taged_web/elasticsearch_control.py;
 python manage.py makemigrations;
 python manage.py migrate;
+python manage.py collectstatic --noinput --settings=taged_web.static_settings;
 python manage.py runserver 0.0.0.0:8000;
---
+++ taged_web/static_settings.py
@@ -0,0 +1,5 @@
+from taged.settings import *
+
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = []

828da247a4319db4eb119c17bfe2edeedc091d04
ig-rudenko <ig.rudenko1@yandex.ru>
2023-12-26 10:58:16 +0300

    v 1.0.15 Сбор файлов статики, при инициализации контейнера.

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 python manage.py makemigrations;
 python manage.py migrate;
+export DJANGO_COLLECT_STATIC=1
+python manage.py collectstatic --noinput
+export DJANGO_COLLECT_STATIC=0
 gunicorn --workers 2 --bind 0.0.0.0:8000 taged.wsgi:application;

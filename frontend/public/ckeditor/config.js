8166bfd286564d364258cc8696704615003dc38f
ig-rudenko <ig.rudenko1@yandex.ru>
2024-10-10 17:26:36 +0300

    v 1.5.2 Автоматическое изменение размера картинки при вставке

--- frontend/public/ckeditor/config.js
+++ frontend/public/ckeditor/config.js
@@ -8,3 +8,24 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
 };
+
+const IMAGES = [];
+
+function setSizes(editor) {
+    const images = editor.document.find('img').$
+    // console.log(images)
+    images.forEach(img => {
+        // console.log("CHECK IMAGES")
+        if (IMAGES.indexOf(img) === -1) {
+            // console.log("SET STYLE")
+            img.setAttribute('style', 'max-width: 100%');
+            IMAGES.push(img)
+        }
+    });
+    setTimeout(() => setSizes(editor), 500)
+}
+
+CKEDITOR.on('instanceReady', evt => {
+    const editor = evt.editor;
+    setSizes(editor)
+});
